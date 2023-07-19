4d87311e1f90df68e6207ef7785797331080838a
Deyanpanayotov <61658611+Deyanpanayotov@users.noreply.github.com>
2023-07-19 17:57:38 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
+from flask import Flask, render_template
+from app import server
 
+app = Flask(__name__)
+
+@app.route('/')
+def render_dash_app():
+    from app import app as dash_app
+    return render_template('index.html', dash_app=dash_app.layout)
+
+if __name__ == '__main__':
+    app.run(debug=True)
